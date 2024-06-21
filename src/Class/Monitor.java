a561dd40a3ed8657728c562b6bbf1cc10e8de9bb
josue2326 <josuerangel212@gmail.com>
2024-06-21 10:39:39 -0500

    Se agrega el proyecto

---
+++ src/Class/Monitor.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+/**
+ *
+ * @author josue
+ */
+public class Monitor {
+    private int id;
+   private String Nombre;
+   private double precio;
+
+    public Monitor() {
+    }
+
+    public Monitor(int id, String Nombre, double precio) {
+        this.id = id;
+        this.Nombre = Nombre;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }  
+}
